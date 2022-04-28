84d723774294565b0f647d805f6e84a7a31e5368
薛辛超 <xinchao.xue@bioon.com>
2022-04-28 10:13:53 +0800

    fix: 404文案

--- src/views/404.vue
+++ src/views/404.vue
@@ -13,7 +13,7 @@
     <div class="bullshit">
       <div class="bullshit__headline">404</div>
       <div class="bullshit__info">
-        请检查您输入的网址是否正确，请点击以下按钮返回主页或者发送错误报告
+        请检查您输入的网址是否正确，请点击以下按钮返回主页或者发送错误报告！
       </div>
       <span @click="go_back" class="bullshit__return-home">返回首页</span>
     </div>
