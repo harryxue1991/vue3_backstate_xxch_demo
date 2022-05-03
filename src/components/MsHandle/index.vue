94e441814bb2f6c3dd35c7b41c7d3d647892838a
薛辛超 <xinchao.xue@bioon.com>
2022-05-03 17:18:01 +0800

    fix: 优化按钮show

--- src/components/MsHandle/index.vue
+++ src/components/MsHandle/index.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <el-button
-      v-for="(item, index) in filterItems"
+      v-for="(item, index) in items"
       :key="index"
       :type="item.type ? item.type : 'text'"
       @click="handleClick(item)"
       :disabled="item.disabled"
+      v-show="item.show ?? true"
       :color="item?.color"
     >
       <span v-text="item.label"></span>
@@ -14,18 +15,12 @@
 </template>
 
 <script setup>
-import { computed } from "vue";
-const props = defineProps({
+defineProps({
   items: {
     type: Array,
     default: () => [],
   },
 });
-
-// 过滤掉不显示的，默认undefined显示
-const filterItems = computed(() =>
-  props.items.filter((i) => i.show === undefined || i.show)
-);
 
 // emit触发事件
 const emit = defineEmits();
